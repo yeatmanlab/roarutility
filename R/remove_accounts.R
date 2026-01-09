4cbc61fab013c7ae124b2e10f545bdd06d543a18
kellywentz <159967652+kellywentz@users.noreply.github.com>
2026-01-08 18:20:40 -0800

    update documentation for remove_accounts

--- R/remove_accounts.R
+++ R/remove_accounts.R
@@ -1,5 +1,18 @@
 #' Remove test, demo, pilot, QA, or NA accounts
 #'
+#' @description
+#' Removes all indicated accounts from the dataframe. Reseearchers can read in
+#'  data and indicate which or all of the following types of accounts they would
+#'  like to remove from the dataframe. The function defaults to removing test,
+#'  demo, pilot, and QA accounts and defaults to not removing NA assessment_pid.
+#'   The function runs through the organization IDs
+#'   (i.e., assigning_districts, etc.). It also uses string detection to
+#'   determine if there are any "test", "pilot", "qa", or "demo" strings
+#'   within the assessment_pid column. Finally, it runs through to determine
+#'   if there are test or demo using the variables is_test_data and is_demo_data
+#'    (if these accounts were chosen to be removed). If selected, the function
+#'    will also remove assessment_pid = NA.
+#'
 #' @param df The dataframe that contains all types of accounts
 #' @param test A Boolean value, TRUE (default) or FALSE, that indicates whether
 #' or not to remove test accounts/participants
